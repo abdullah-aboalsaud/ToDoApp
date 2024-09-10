000031f3bd31cd8cb804715f72d4c678d08bf042
Abdullah-Aboalsaud <abdullah.abo.alssaued@gmail.com>
2024-09-10 17:15:25 +0300

    add: initAdapter for language and mode. add: onModeTvClick

--- app/src/main/java/com/example/todoapp/ui/fragments/SettingFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/fragments/SettingFragment.kt
@@ -6,12 +6,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.todoapp.R
 import com.example.todoapp.base.BaseFragment
 import com.example.todoapp.databinding.FragmentSettingBinding
 import com.example.todoapp.utils.DARK
 import com.example.todoapp.utils.ENGLISH_CODE
 import com.example.todoapp.utils.LIGHT
+import com.example.todoapp.utils.applyModeChange
 import com.example.todoapp.utils.getCurrentDeviceLanguageCode
 import com.example.todoapp.utils.showBottomAppBarViews
 import com.example.todoapp.utils.showToolbarSettingTitle
@@ -30,8 +32,41 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>() {
         setInitialLanguageState()
         setInitialModeState()
 
+        initLanguageTvAdapter()
+        initModeTvAdapter()
+
+        onLanguageTvClick()
+        onModeTvClick()
 
     }
+
+    private fun onModeTvClick() {
+        binding.autoCompleteTVModes.setOnItemClickListener { adapterView, view, position, id ->
+            val selectedMode = adapterView.getItemAtPosition(position).toString()
+            binding.autoCompleteTVModes.setText(selectedMode)
+            val isDark = (selectedMode == getString(R.string.dark))
+            applyModeChange(isDark)
+        }
+    }
+
+
+
+    private fun onLanguageTvClick() {
+
+    }
+
+    private fun initLanguageTvAdapter() {
+        val languages = resources.getStringArray(R.array.languages).toList()
+        val languagesAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, languages)
+        binding.autoCompleteTVLanguages.setAdapter(languagesAdapter)
+    }
+
+    private fun initModeTvAdapter() {
+        val modes = resources.getStringArray(R.array.modes).toList()
+        val modesAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, modes)
+        binding.autoCompleteTVModes.setAdapter(modesAdapter)
+    }
+
 
     private fun setInitialModeState() {
         val currentMode = resources.configuration.uiMode and UI_MODE_NIGHT_MASK
@@ -46,9 +81,9 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>() {
     }
 
     private fun changeModeIcon(mode: String) {
-        if (mode == LIGHT){
+        if (mode == LIGHT) {
             binding.modeTil.setStartIconDrawable(R.drawable.ic_light_mode)
-        }else{
+        } else {
             binding.modeTil.setStartIconDrawable(R.drawable.ic_dark)
         }
     }
--- app/src/main/java/com/example/todoapp/utils/utils.kt
+++ app/src/main/java/com/example/todoapp/utils/utils.kt
@@ -1,7 +1,15 @@
 package com.example.todoapp.utils
 
 import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 
 fun getCurrentDeviceLanguageCode(context: Context):String{
     return context.resources.configuration.locales[0].language
+}
+
+fun applyModeChange(isDark: Boolean) {
+    if (isDark)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+    else
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 }
